35beeab2fbcd747c5712d9d79799d52c232fac4f
zacktagnan <zacktagnan@gmail.com>
2024-05-05 01:31:30 +0200

    Habits_CreateTest-Redirect.a.INDEX.desde.STORE

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Habit;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -14,7 +15,8 @@ class HabitController extends Controller
         return view('habits.index', compact('habits'));
     }
 
-    public function store()
+    public function store(): RedirectResponse
     {
+        return redirect()->route('habits.index');
     }
 }
