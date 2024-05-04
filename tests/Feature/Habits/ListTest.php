2bf001dbbbd688cebc0ad245ae06231d983c1c47
zacktagnan <zacktagnan@gmail.com>
2024-05-05 01:02:33 +0200

    Habits_ListTest-Refactorizar-Import.de.RefreshDatabase.en.TestCase

--- tests/Feature/Habits/ListTest.php
+++ tests/Feature/Habits/ListTest.php
@@ -5,12 +5,9 @@ namespace Tests\Feature\Habits;
 use Tests\TestCase;
 use App\Models\Habit;
 use PHPUnit\Framework\Attributes\Group;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ListTest extends TestCase
 {
-    use RefreshDatabase;
-
     #[Group('habits')]
     #[Group('habits_list')]
     public function test_the_habits_index_view_can_be_render_with_list(): void
