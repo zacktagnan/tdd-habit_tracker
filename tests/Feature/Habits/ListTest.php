ea962d2bae5ac7b2bc7007ff3c9d1a80a77eac4d
zacktagnan <zacktagnan@gmail.com>
2024-05-04 23:45:28 +0200

    Habits_ListTest-MigraciOn.y.RefreshDatabase

--- tests/Feature/Habits/ListTest.php
+++ tests/Feature/Habits/ListTest.php
@@ -5,9 +5,12 @@ namespace Tests\Feature\Habits;
 use Tests\TestCase;
 use App\Models\Habit;
 use PHPUnit\Framework\Attributes\Group;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ListTest extends TestCase
 {
+    use RefreshDatabase;
+
     #[Group('habits')]
     #[Group('habits_list')]
     public function test_the_habits_index_view_can_be_render_with_list(): void
