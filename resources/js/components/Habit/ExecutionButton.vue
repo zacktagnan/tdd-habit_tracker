682e26e6ea44a4e9b47d7b3fcd527a44b9763135
zacktagnan <zacktagnan@gmail.com>
2024-05-06 23:30:13 +0200

    Habit_ExecutionButton.test-Elemento.con.ID.execute

--- resources/js/components/Habit/ExecutionButton.vue
+++ resources/js/components/Habit/ExecutionButton.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-
+    <div id="execute" class="flex basis-1/4 justify-center p-2.5">
+        <button
+            class="bg-primary-600 hover:bg-primary-400 text-white font-semibold text-2xl px-4 py-2 rounded-lg">+1</button>
     </div>
 </template>
