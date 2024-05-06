b71fec479cc0aed3fc5db1564580a16bed086bd9
zacktagnan <zacktagnan@gmail.com>
2024-05-06 15:26:15 +0200

    TailwindCSS-Instalado.y.configurado

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
