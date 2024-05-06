86eba3fde57d37e2df52239e501033f61b05c2ba
zacktagnan <zacktagnan@gmail.com>
2024-05-06 22:48:07 +0200

    Habit_Info-Uso.de.PROP.name

--- resources/js/components/Habit/Info.vue
+++ resources/js/components/Habit/Info.vue
@@ -1,6 +1,14 @@
+<script setup>
+defineProps({
+    name: {
+        type: String,
+    }
+})
+</script>
+
 <template>
     <div class="flex basis-1/2 flex-col">
-        <h2 id="name" class="text-base font-semibold">Beber agua</h2>
+        <h2 id="name" class="text-base font-semibold">{{ name }}</h2>
         <small class="font-normal text-gray-400">1 / 3 veces</small>
     </div>
 </template>
