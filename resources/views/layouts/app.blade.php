6d18938b828763ea6386b7405205a6aa1bbcd88f
zacktagnan <zacktagnan@gmail.com>
2024-05-06 15:42:48 +0200

    AppLayout.creado

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-gray-100">
+    <div class="container mx-auto px-4">
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
