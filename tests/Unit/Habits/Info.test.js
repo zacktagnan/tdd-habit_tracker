6682d1bd112038011bdb22266041fb9c5516df9b
zacktagnan <zacktagnan@gmail.com>
2024-05-06 22:48:58 +0200

    Habit_Info.test-Wrapper.puesto.en.modo.general

--- tests/Unit/Habits/Info.test.js
+++ tests/Unit/Habits/Info.test.js
@@ -1,15 +1,19 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import HabitInfo from '@/components/Habit/Info.vue'
 
 describe('HabitInfo.vue', () => {
-    it('displays the habit name', () => {
-        const wrapper = mount(HabitInfo, {
+    let wrapper = null
+
+    beforeEach(() => {
+        wrapper = mount(HabitInfo, {
             props: {
                 name: 'Beber agua'
             }
         })
+    })
 
+    it('displays the habit name', () => {
         expect(wrapper.find('#name').text()).toBe('Beber agua')
     })
 })
