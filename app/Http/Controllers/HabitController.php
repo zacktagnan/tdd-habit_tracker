d84833ce4a468e4bc12a7cfef2eb971532cb0619
zacktagnan <zacktagnan@gmail.com>
2024-05-06 13:52:19 +0200

    Habits_UpdateTest-ValidaciOn.mUltiple.con.dataProvider

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\HabitStoreRequest;
+use App\Http\Requests\HabitUpdateRequest;
 use App\Models\Habit;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class HabitController extends Controller
@@ -22,7 +22,7 @@ class HabitController extends Controller
         return redirect()->route('habits.index');
     }
 
-    public function update(Request $request, Habit $habit): RedirectResponse
+    public function update(HabitUpdateRequest $request, Habit $habit): RedirectResponse
     {
         $habit->update($request->all());
         return redirect()->route('habits.index');
--- tests/Feature/Habits/UpdateTest.php
+++ tests/Feature/Habits/UpdateTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Habits;
 
 use Tests\TestCase;
 use App\Models\Habit;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 
 #[Group('habits')]
@@ -31,5 +32,36 @@ class UpdateTest extends TestCase
         $response->assertRedirect(route('habits.index'));
         $this->assertDatabaseHas('habits', $habitUpdated);
         // ------------------------------------------------
+    }
+
+    #[DataProvider('provideBadDataProvider')]
+    public function test_habit_update_validation(string $columnToValidate, array $habit): void
+    {
+        $habitStored = Habit::factory()->create();
+
+        $response = $this->put(route('habits.update', $habitStored), $habit);
+        $response->assertSessionHasErrors([$columnToValidate]);
+    }
+
+    public static function provideBadDataProvider(): array
+    {
+        $habit = Habit::factory()->make();
+
+        return [
+            'about name column' => [
+                'name',
+                [
+                    ...$habit->toArray(),
+                    'name' => null,
+                ],
+            ],
+            'about times_per_day column' => [
+                'times_per_day',
+                [
+                    ...$habit->toArray(),
+                    'times_per_day' => null,
+                ],
+            ],
+        ];
     }
 }
