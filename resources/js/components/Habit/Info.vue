cfed116f3807e58b0e19381959b8a908d23df338
zacktagnan <zacktagnan@gmail.com>
2024-05-06 22:29:36 +0200

    Habit_Info.test-Elemento.con.ID.name

--- resources/js/components/Habit/Info.vue
+++ resources/js/components/Habit/Info.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-
+    <div class="flex basis-1/2 flex-col">
+        <h2 id="name" class="text-base font-semibold">Beber agua</h2>
+        <small class="font-normal text-gray-400">1 / 3 veces</small>
     </div>
 </template>
