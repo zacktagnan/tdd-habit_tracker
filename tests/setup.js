267a664f0b5eb4e7b0dc9a2a4d6d12643f2ae80d
zacktagnan <zacktagnan@gmail.com>
2024-05-12 23:37:46 +0200

    Error.corregido.sobre.HANDLER.para.peticiones.POST

--- tests/setup.js
+++ tests/setup.js
@@ -17,12 +17,31 @@ const habits = {
 export const requestHandlers = [
     // rest.get('http://localhost:3000/api/habits', (req, res, ctx) => {
     //     return res(ctx.status(200), ctx.json(habits))
-    // })
-    http.get('http://localhost:3000/api/habits', () => {
+    // }),
+    // o
+    // rest.get('/api/habits', (req, res, ctx) => {
+    //     return res(ctx.status(200), ctx.json(habits))
+    // }),
+    // ----------------------------------------------------------------
+    // http.get('http://localhost:3000/api/habits', () => {
+    //     return HttpResponse.json(habits, {
+    //         status: 200,
+    //     })
+    // }),
+    // o
+    http.get('/api/habits', () => {
         return HttpResponse.json(habits, {
             status: 200,
         })
-    })
+    }),
+    // rest.post('/api/habits/:habit/execution', (req, res, ctx) => {
+    //     return res(ctx.status(200), ctx.json(habits))
+    // }),
+    http.post('/api/habits/:habit/execution', () => {
+        return HttpResponse.json({}, {
+            status: 200,
+        })
+    }),
 ]
 
 const server = setupServer(...requestHandlers)
